f12217043be07b063b1cbc582d97295a08e4dfb6
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2024-05-10 17:33:25 +0800

    feat: gitlab commit could do on single repository

--- app/gitlab/x-sync-projects.sh
+++ app/gitlab/x-sync-projects.sh
@@ -7,29 +7,59 @@ ROOT=${PWD}
 source ./src/utils.sh
 source ./src/git.sh
 
+# Declare repository
+TARGET_REPOSITORY=${1}
+
 # Execute script
+if [ -z ${TARGET_REPOSITORY} ];
+then
+    ## Fetch all repository to main branch
+    echo-i "Initial repository"
+    for repo in $(ls code/);
+    do
+        git-init RD ${repo} main
+    done
 
-## Fetch all repository to main branch
-for repo in $(ls code/);
-do
-    git-init RD ${repo} main
-done
+    ## Copy code to .git repository
+    echo-i "Copy code to .git"
+    rsync -avh --delete --exclude ".git/" --exclude='cache/' code/* .git/
 
-## Copy code to .git repository
-echo-i "Copy code to .git"
-rsync -avh --delete --exclude ".git/" --exclude='cache/' code/* .git/
-
-## Push new commit into gitlab, if code has difference.
-echo-i "Push code into gitlab"
-for repo in $(ls .git/);
-do
-    cd .git/${repo}
-    if [ $(git commit | grep "nothing to commit" | wc -l) -eq 0 ];
+    ## Push new commit into gitlab, if code has difference.
+    echo-i "Push code into gitlab"
+    for repo in $(ls .git/);
+    do
+        cd .git/${repo}
+        if [ $(git commit | grep "nothing to commit" | wc -l) -eq 0 ];
+        then
+            echo ${repo}, add new commit
+            git-tree-add-commit ${repo} "feat: new feature from code `date "+%Y.%m.%d.%H%M%S"`"
+        else
+            echo ${repo}, nothing to commit
+        fi
+        cd ${ROOT}
+    done
+else
+    if [ -d code/${TARGET_REPOSITORY} ];
     then
-        echo ${repo}, add new commit
-        git-tree-add-commit ${repo} "feat: new feature from code `date "+%Y.%m.%d.%H%M%S"`"
+        repo=${TARGET_REPOSITORY}
+        ## Fetch all repository to main branch
+        echo-i "Initial repository"
+        git-init RD ${repo} main
+        ## Copy code to .git repository
+        echo-i "Copy code to .git"
+        rsync -avh --delete --exclude ".git/" --exclude='cache/' --exclude='dist/' --exclude='node_modules/' code/${repo}/*  .git/${repo}
+        ## Push new commit into gitlab, if code has difference.
+        echo-i "Push code into gitlab"
+        cd .git/${repo}
+        if [ $(git commit | grep "nothing to commit" | wc -l) -eq 0 ];
+        then
+            echo ${repo}, add new commit
+            git-tree-add-commit ${repo} "feat: new feature from code `date "+%Y.%m.%d.%H%M%S"`"
+        else
+            echo ${repo}, nothing to commit
+        fi
+        cd ${ROOT}
     else
-        echo ${repo}, nothing to commit
+        echo "Repository ${TARGET_REPOSITORY} is not exist."
     fi
-    cd ${ROOT}
-done
+fi
--- conf/cli/gitlab-commit.bat
+++ conf/cli/gitlab-commit.bat
@@ -10,10 +10,13 @@ goto end
 @rem ------------------- declare function -------------------
 
 :action
-    docker exec -ti %PROJECT_NAME%-infra-gitlab bash x-sync-projects.sh
+    docker exec -ti %PROJECT_NAME%-infra-gitlab bash x-sync-projects.sh %COMMON_ARGS_VALUE%
     goto end
 
 :args
+    set COMMON_ARGS_KEY=%1
+    set COMMON_ARGS_VALUE=%2
+    if "%COMMON_ARGS_KEY%"=="--repo" (set TARGET_REPOSITORY=%COMMON_ARGS_VALUE%)
     goto end
 
 :short
@@ -26,6 +29,7 @@ goto end
     echo.
     echo Options:
     echo      --help, -h        Show more information with command.
+    echo      --repo            Select repository ( defaul all repository).
     call %CLI_SHELL_DIRECTORY%\utils\tools.bat command-description %~n0
     goto end
 
