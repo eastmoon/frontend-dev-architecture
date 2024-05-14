c7e9cceffb8d8608ef88892158fd76e0faff4f07
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2024-05-14 11:03:50 +0800

    feat: modify view style in page

--- src/page/app/src/App.js
+++ src/page/app/src/App.js
@@ -1,31 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import clibs from 'clibs';
 
-function App() {
-  console.log(clibs);
-  console.log(clibs.pattern);
-  clibs.utils.echoi("12345");
-  clibs.utils.echow("5678");
-  clibs.utils.echoe("0912");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 123
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //
+  constructor(props) {
+      super(props);
+      console.log(clibs);
+      console.log(clibs.pattern);
+      clibs.utils.echoi("1234");
+      clibs.utils.echow("5678");
+      clibs.utils.echoe("0912");
+      this.state = {
+          initial: false
+      };
+  }
+  // Component render
+  renderInit() {
+      return (
+          <div className="w-full h-full">
+              <div className="p-2">
+                  <div className="flex items-center p-4 mb-4 text-sm border rounded-lg bg-gray-800 text-green-400 border-green-800">
+                      <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                      </svg>
+                      <div>
+                          系統啟動中 ...
+                      </div>
+                  </div>
+              </div>
+          </div>
+      )
+  }
+  renderApp() {
+      return (
+          <div className="w-full h-full">
+              Hello world !!!!
+          </div>
+      )
+  }
+  render() {
+    return (
+      <div className="p-2 App">
+        <h1 className="text-3xl font-bold">Frontend Develop architecture</h1>
+        { ! this.state.initial ? this.renderInit() : this.renderApp()}
+      </div>
+    );
+  }
 }
 
 export default App;
