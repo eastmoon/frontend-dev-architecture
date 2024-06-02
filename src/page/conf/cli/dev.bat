a408fe8d9264ee190445405d102d700edc2021e1
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2024-06-02 18:36:10 +0800

    feat: add --hostname and --detach optations

--- src/page/conf/cli/dev.bat
+++ src/page/conf/cli/dev.bat
@@ -4,6 +4,7 @@
 @rem ------------------- declare variable -------------------
 if not defined TARGET_PROJECT_COMMAND (set TARGET_PROJECT_COMMAND=node)
 if not defined TARGET_PROJECT_DEV_SERVER_PORT (set TARGET_PROJECT_DEV_SERVER_PORT=3000)
+if not defined TARGET_PROJECT_DEV_SERVER_HOSTNAME (set TARGET_PROJECT_DEV_SERVER_HOSTNAME=page)
 
 @rem ------------------- execute script -------------------
 call :%*
@@ -13,7 +14,11 @@ goto end
 
 :action
     echo Start project %TARGET_PROJECT% develop server
-    call %CLI_SHELL_DIRECTORY%\utils\libs.bat exec-docker %TARGET_PROJECT_COMMAND% %TARGET_PROJECT_DEV_SERVER_PORT%
+    call %CLI_SHELL_DIRECTORY%\utils\libs.bat exec-docker ^
+        %TARGET_PROJECT_COMMAND% ^
+        %TARGET_PROJECT_DEV_SERVER_PORT% ^
+        %TARGET_PROJECT_DEV_SERVER_HOSTNAME% ^
+        %TARGET_PROJECT_DEV_SERVER_IN_BACKGROUND%
     goto end
 
 :args
@@ -21,6 +26,8 @@ goto end
     set COMMON_ARGS_VALUE=%2
     if "%COMMON_ARGS_KEY%"=="--into" (set TARGET_PROJECT_COMMAND=bash)
     if "%COMMON_ARGS_KEY%"=="--port" (set TARGET_PROJECT_DEV_SERVER_PORT=%2)
+    if "%COMMON_ARGS_KEY%"=="--hostname" (set TARGET_PROJECT_DEV_SERVER_HOSTNAME=%2)
+    if "%COMMON_ARGS_KEY%"=="--detach" (set TARGET_PROJECT_DEV_SERVER_IN_BACKGROUND=--detach)
     goto end
 
 :short
@@ -34,7 +41,9 @@ goto end
     echo Options:
     echo      --help, -h        Show more command information.
     echo      --into            When container startup then going to container and don't startup dev-server.
-    echo      --port            Setting dev-server port ( Default 3000 ).
+    echo      --port            Setting dev-server port ( Default %TARGET_PROJECT_DEV_SERVER_PORT% ).
+    echo      --hostname        Setting dev-server hostname ( Default %TARGET_PROJECT_DEV_SERVER_HOSTNAME% ).
+    echo      --detach          Run container in background.
     call %CLI_SHELL_DIRECTORY%\utils\tools.bat command-description %~n0
     goto end
 
--- src/page/conf/cli/utils/libs.bat
+++ src/page/conf/cli/utils/libs.bat
@@ -8,6 +8,14 @@ goto end
 @rem ------------------- declare function -------------------
 
 :exec-docker
+    @rem Declare variable
+    set VAR_SRV_CMD=%1
+    set VAR_SRV_PORT=%2
+    set VAR_SRV_HOSTNAME=%3
+    set VAR_SRV_STATE=%4
+    if not defined VAR_SRV_STATE (set VAR_SRV_STATE%=-ti)
+
+    @rem Initial directory
     echo ^> Create cache directory
     if NOT EXIST %CLI_DIRECTORY%\cache\dist (
         mkdir %CLI_DIRECTORY%\cache\dist
@@ -17,6 +25,7 @@ goto end
         mkdir %CLI_DIRECTORY%\cache\node_modules
     )
 
+    @rem Create service image with Dockerfile
     echo ^> Build image
     set PROJECT_NAME=frontend-dev-architecture
     set DOCKER_IMAGE_NAME=react.sdk:%PROJECT_NAME%
@@ -24,20 +33,22 @@ goto end
         -t %DOCKER_IMAGE_NAME%^
         ./conf/docker/react
 
+    @rem Startup service with parameter
     echo ^> Startup service
     set INFRA_DOCKER_NETWORK=frontend-dev-architecture-network
-    set DOCKER_CONTAINER_NAME=%PROJECT_NAME%-react-page-dev
+    set DOCKER_CONTAINER_NAME=%PROJECT_NAME%-%VAR_SRV_HOSTNAME%
     docker rm -f %DOCKER_CONTAINER_NAME%
-    docker run -ti --rm ^
+    docker run %VAR_SRV_STATE% --rm ^
         -v %CLI_DIRECTORY%\app:/app ^
         -v %CLI_DIRECTORY%\cache\dist:/app/build ^
         -v %CLI_DIRECTORY%\cache\node_modules:/app/node_modules ^
-        -e PORT=%2 ^
-        -p %2:%2 ^
+        -e PORT=%VAR_SRV_PORT% ^
+        -h %VAR_SRV_HOSTNAME% ^
+        -p %VAR_SRV_PORT%:%VAR_SRV_PORT% ^
         -w "/app" ^
         --network %INFRA_DOCKER_NETWORK% ^
         --name %DOCKER_CONTAINER_NAME% ^
-        %DOCKER_IMAGE_NAME% %1
+        %DOCKER_IMAGE_NAME% %VAR_SRV_CMD%
     goto end
 
 @rem ------------------- End method-------------------
